81c76d6b5a80327d29be7850ff8987499395a4b9
Carlos Vidal <cvidals.dev@gmail.com>
2025-12-05 18:59:56 +0100

    Fin sección 13

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,45 @@
+import { Link, useLocation } from "react-router";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+export function CustomMenu() {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList className="flex gap-2">
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive("/") && "bg-slate-200 rounded-md", "p-2")}
+          >
+            <Link to="/">Inicio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        {/* Search */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(
+              isActive("/search") && "bg-slate-200 rounded-md",
+              "p-2"
+            )}
+          >
+            <Link to="/search">Buscar superhéroes</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
 
 export const SearchPage = () => {
   return (
@@ -9,6 +10,16 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         subtitle="Descubre, explora y administra tus superhéroes y villanos favoritos"
       />
+
+      <CustomBreadcrumbs
+        currentPage="Buscador de héroes"
+        // breadcrumbs={[
+        //   { label: "Home1", to: "/" },
+        //   { label: "Home2", to: "/" },
+        //   { label: "Home3", to: "/" },
+        // ]}
+      />
+
       {/* Stats DashBoard */}
       <HeroStats />
 
